5b3598baf6fcbb21d99cef394fa79f7cbdf82cb9
Rohit Tudu <rohittudu181@gmail.com>
2023-10-26 16:06:52 +0530

    UI Changes

--- lib/components/square_tile.dart
+++ lib/components/square_tile.dart
@@ -1,25 +1,29 @@
 import 'package:flutter/material.dart';
 
 class SquareTile extends StatelessWidget {
-
+  Function()? onTap;
   final String imagePath;
-  const SquareTile({
+
+  SquareTile({
     super.key,
-    required this.imagePath
+    required this.imagePath,
+    required this.onTap
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.white),
-        borderRadius: BorderRadius.circular(16),
-        color: Colors.grey[200]
-      ),
-      child: Image.asset(
-        imagePath,
-        height: 40 ,
+    return GestureDetector(
+      child: Container(
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.white),
+          borderRadius: BorderRadius.circular(16),
+          color: Colors.grey[200]
+        ),
+        child: Image.asset(
+          imagePath,
+          height: 40 ,
+        ),
       ),
     );
   }
